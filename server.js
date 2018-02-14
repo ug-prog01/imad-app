f126405639a98b193ddc4a61bf4c3640a5fda840
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-14 21:20:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var number1 = {
+    title: 'Number 1 Guys!',
+    heading: 'Some Random Shit',
+    date: 'Again some Random shit',
+    content: `Again Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random DataAgain Some more Random Data`
+};
+
+function createTemp(data) {
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmlTemp = `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+            <div>
+                <a href = "/">Home</a>
+            </div>
+            <hr/>
+            <h4>
+                ${heading}
+            </h4>
+            <div>
+                ${date}
+            </div>
+            <div>
+                <p>
+                    ${content}
+                </p>
+                <p>
+                    ${content}
+                </p>
+                <p>
+                    ${content}
+                </p>
+            </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemp;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemp(number1));
 });
 
 app.get('/number1', function(req, res) {
