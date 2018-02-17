c7c7b2309082a529c7dc8d72f32b4fbd26751a21
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-17 16:33:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ return htmlTemp;
 var counter = 0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
-    res.send(counter, toString());
+    res.send(counter.toString());
 })
 
 app.get('/', function (req, res) {
