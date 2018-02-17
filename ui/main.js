d471465b75cad78a732275c44c3e9ae6b35ae25d
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-17 16:07:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('box').innerHTML;
 console.log(element);
 
 var element1 = document.getElementById('enter').addEventListener('click', function(element){
-    document.getElementById('maintext').innerHTML = element;
+    document.getElementById('maintext').innerHTML = element.innerHTML;
 })
 
 
