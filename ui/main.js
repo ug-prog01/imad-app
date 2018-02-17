4b18a4a44de613c7eade307f890c80f545c001f2
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-17 16:49:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ button.onclick = function() {
         }
     };
     
-    request.open('GET', 'http://ug19991.imad.hasura-app.io/', true);
+    request.open('GET', 'http://ug19991.imad.hasura-app.io/counter', true);
     request.send(null);
     
     
