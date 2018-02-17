91d42c5237553b1a763b4d10489de2909b93de1b
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-17 16:12:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('polo');
 var element = document.getElementById('box');
 console.log(element.innerHTML);
 var element1 = document.getElementById('enter').addEventListener('click', function(){
-    document.getElementById('maintext').innerHTML = element.innerHTML;
+    document.getElementById('maintext').innerHTML = 'element.innerHTML';
 })
 
 
