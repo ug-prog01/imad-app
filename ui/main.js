0fdb9c93a61f3b3a851746ee531400a8197a692b
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-17 16:38:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ console.log('Loaded!');
 //    document.getElementById('maintext').innerHTML = 'element.innerHTML';
 //})
 
-var img = document.getElementById('dino');
+/*var img = document.getElementById('dino');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 5;
@@ -15,5 +15,13 @@ function moveRight () {
 
 img.onclick = function() {
     var delay = setInterval(moveRight, 20)
-}
+}*/
 
+var button = document.getElementById("clickme");
+button.onclick = function() {
+    
+    
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+}
