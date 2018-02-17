7692298b3aa223a9f770421b4374c8b834df2139
ug-prog01 <31947259+ug-prog01@users.noreply.github.com>
2018-02-17 16:32:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,6 +67,12 @@ var htmlTemp = `<html>
 return htmlTemp;
 }
 
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter, toString());
+})
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
